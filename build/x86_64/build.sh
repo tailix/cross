537ce7fa9fdb70ac1cdfb8a905015df6f1839b26
Alex Kotov <kotovalexarian@gmail.com>
2022-01-17 23:47:26 +0500

    Fix typos

--- build/x86_64/build.sh
+++ build/x86_64/build.sh
@@ -13,11 +13,11 @@ PATH="$ROOT/bin:$PATH"
 if [ -z "$J" ]; then J='1'; fi
 
 cd "$REPO/build/$ARCH/binutils"
-"$REPO/binutils-2.37/configure" --target="$TARGET" --prefix="$ROOT" --with-sysroot --disable-nls --disable-werror
+"$REPO/src/binutils-2.37/configure" --target="$TARGET" --prefix="$ROOT" --with-sysroot --disable-nls --disable-werror
 make -j"$J"
 make install
 
 cd "$REPO/build/$ARCH/gcc"
-"$REPO/gcc-11.2.0/configure" --target="$TARGET" --prefix="$ROOT" --disable-nls --enable-languages=c,c++ --without-headers
+"$REPO/src/gcc-11.2.0/configure" --target="$TARGET" --prefix="$ROOT" --disable-nls --enable-languages=c,c++ --without-headers
 make -j"$J" all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
